0b65ff7f1930bc58721e5cdb4799e64c4372fa82
Martin Polanka <PolankaMartin@gmail.com>
2019-10-29 21:15:35 +0100

    Change behaviour of evaluator, now matching rules are found (matching with resource and action) and then the allowance of the first defined rule is returned

--- src/main/java/cz/polankam/security/acl/AclPermissionEvaluator.java
+++ src/main/java/cz/polankam/security/acl/AclPermissionEvaluator.java
@@ -1,5 +1,6 @@
 package cz.polankam.security.acl;
 
+import cz.polankam.security.acl.exceptions.PermissionException;
 import cz.polankam.security.acl.exceptions.ResourceNotFoundException;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
@@ -8,7 +9,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Custom permission evaluator used for 'hasPermission' expressions within
@@ -65,13 +70,15 @@ public class AclPermissionEvaluator implements PermissionEvaluator {
                 continue;
             }
 
-            for (PermissionRule permissionRule : role.getPermissionRules(targetResource)) {
-                if (permissionRule.getResource().equals(targetResource) &&
-                        permissionRule.getAction().equals(permissionString) &&
-                        permissionRule.isAllowed() &&
-                        permissionRule.getCondition() == null) {
-                    return true;
+            List<PermissionRule> rules = findMatching(role.getPermissionRules(targetResource), targetResource, permissionString);
+            Optional<PermissionRule> firstRule = rules.stream().findFirst();
+            if (firstRule.isPresent()) {
+                if (firstRule.get().getCondition() == null) {
+                    throw new PermissionException("ABAC permission rule for resource '" + targetResource + 
+                        "' and action '" + permissionString + "' was used in non-ABAC context");
                 }
+
+                return firstRule.get().isAllowed();
             }
         }
         return false;
@@ -106,38 +113,50 @@ public class AclPermissionEvaluator implements PermissionEvaluator {
                 continue;
             }
 
-            for (PermissionRule permissionRule : role.getPermissionRules(targetType)) {
-                if (permissionRule.getResource().equals(targetType) &&
-                        permissionRule.getAction().equals(permissionString) &&
-                        permissionRule.isAllowed()) {
+            List<PermissionRule> rules = findMatching(role.getPermissionRules(targetType), targetType, permissionString);
+            Optional<PermissionRule> firstRule = rules.stream().findFirst();
+            if (firstRule.isPresent()) {
+                PermissionRule rule = firstRule.get();
 
-                    // we have to find resource repository, because we were
-                    // given resource identification, after that resource is
-                    // acquired from the repository and evaluated in specified
-                    // condition
-                    IResourceRepository repository = permissionsService.getResource(permissionRule.getResource());
-                    Optional<Object> resource = repository.findById(targetId);
-                    if (!resource.isPresent()) {
-                        throw new ResourceNotFoundException("Resource with identification '" + targetId + "' not found");
+                // we have to find resource repository, because we were
+                // given resource identification, after that resource is
+                // acquired from the repository and evaluated in specified
+                // condition
+                IResourceRepository repository = permissionsService.getResource(rule.getResource());
+                Optional<Object> resource = repository.findById(targetId);
+                if (!resource.isPresent()) {
+                    throw new ResourceNotFoundException("Resource with identification '" + targetId + "' not found");
+                }
+
+                if (rule.getCondition() != null) {
+                    // condition was given, so evaluate it
+                    if (rule.getCondition().test(user, resource.get())) {
+                        return rule.isAllowed();
                     }
 
-                    if (permissionRule.getCondition() != null) {
-                        // condition was given, so evaluate it
-                        if (permissionRule.getCondition().test(user, resource.get())) {
-                            return true;
-                        }
-
-                        // if condition was false, we have to continue
-                        // evaluating another rules, because some of them might
-                        // be truthy and grant access to resource
-                    } else {
-                        // condition was not given, so the behaviour is the same
-                        // as for regular id-less permission check, allow it
-                        return true;
-                    }
+                    // if condition was false, we have to continue
+                    // evaluating another rules, because some of them might
+                    // be truthy and grant access to resource
+                } else {
+                    // condition was not given, so the behaviour is the same
+                    // as for regular id-less permission check, allow it
+                    return rule.isAllowed();
                 }
             }
         }
         return false;
+    }
+
+    /**
+     * Find all matching rules with given resource and action.
+     * @param rules source rules
+     * @param resource resource which should be found
+     * @param action action which should be found
+     * @return filtered collection of matching rules
+     */
+    private List<PermissionRule> findMatching(Collection<PermissionRule> rules, String resource, String action) {
+        return rules.stream().filter(rule -> 
+            Objects.equals(rule.getResource(), resource) && Objects.equals(rule.getAction(), action)
+        ).collect(Collectors.toList());
     }
 }
